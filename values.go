b611830ab0d52fe9eb86b38774a54441833548c1
happymca <agrawalamit85@gmail.com>
2024-10-15 06:53:05 +0000

    updated values file

--- values.go
+++ values.go
@@ -1,5 +1,18 @@
 package main
 
+<<<<<<< HEAD
+import "fmt"
+
+func main() {
+
+	//strings	
+	fmt.Println ("go" + "lang")
+
+	//intergers and float
+	fmt.Println("1+1 = ",1+1)
+	fmt.Println("7.0/3.0=", 7.0/3.0)
+}
+=======
 import "fmt"
 
 func main(){
@@ -10,3 +23,4 @@ func main(){
 	//integers and floats
 	fmt.Println("1+1 ",1+1)
 }
+>>>>>>> a11b556e940411c4c7827e3bd6086e35750ef15b
