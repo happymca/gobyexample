4c9f448d483bd27af8e548a611e581c78c61761e
happymca <agrawalamit85@gmail.com>
2024-10-15 07:14:27 +0000

    addeded more code

---
+++ variables.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	//string
+	var a = "initial"
+	fmt.Println(a)
+
+	//int
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+
+	//boolean
+	var d = true
+	fmt.Println(d)
+
+	// default int, boolean, float, string
+	var e int
+	fmt.Println(e)
+	var f bool
+	var g float32
+	var h string
+
+	fmt.Println (f,g,h)
+
+
+	//short assignment
+	i := "apple"
+    fmt.Println(i)
+
+
+}
