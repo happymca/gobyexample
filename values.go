557c9b144ec360dd54768900c7cdcb126ae16abe
happymca <agrawalamit85@gmail.com>
2024-10-15 06:54:50 +0000

    updated values.go

--- values.go
+++ values.go
@@ -1,6 +1,5 @@
 package main
 
-<<<<<<< HEAD
 import "fmt"
 
 func main() {
@@ -11,16 +10,8 @@ func main() {
 	//intergers and float
 	fmt.Println("1+1 = ",1+1)
 	fmt.Println("7.0/3.0=", 7.0/3.0)
+
+	fmt.Println(true && false)
+    fmt.Println(true || false)
+    fmt.Println(!true)
 }
-=======
-import "fmt"
-
-func main(){
-
-	//strings
-	fmt.Println("thank "+"you")
-
-	//integers and floats
-	fmt.Println("1+1 ",1+1)
-}
->>>>>>> a11b556e940411c4c7827e3bd6086e35750ef15b
